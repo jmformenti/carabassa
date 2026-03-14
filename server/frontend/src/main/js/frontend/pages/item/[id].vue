d5038335cfdbdca43635d5c5d491813d28cc1617
jmformenti <jmformenti@gmail.com>
2026-03-14 20:13:49 +0100

    feat(ui):Unified icons (#45)

--- server/frontend/src/main/js/frontend/pages/item/[id].vue
+++ server/frontend/src/main/js/frontend/pages/item/[id].vue
@@ -5,13 +5,36 @@
         <div class="d-flex align-center mb-4">
           <v-btn icon="mdi-arrow-left" variant="text" class="mr-4" @click="$router.back()" />
           <h2 class="text-h4 mr-2">{{ item.filename }}</h2>
+          <v-tooltip text="Download" location="top">
+            <template #activator="{ props }">
+              <v-btn
+                v-bind="props"
+                icon="mdi-download"
+                variant="text"
+                color="orange-darken-2"
+                :href="$carabassa.getItemContentURL(item.id)"
+                target="_blank"
+              />
+            </template>
+          </v-tooltip>
+          <v-tooltip text="Delete" location="top">
+            <template #activator="{ props }">
+              <v-btn
+                v-bind="props"
+                icon="mdi-delete"
+                variant="text"
+                color="orange-darken-2"
+                @click="confirmDeleteItem"
+              />
+            </template>
+          </v-tooltip>
           <v-tooltip text="Copy internal link" location="top">
             <template #activator="{ props }">
               <v-btn
                 v-bind="props"
                 icon="mdi-link"
                 variant="text"
-                color="primary"
+                color="orange-darken-2"
                 @click="copyLink"
               />
             </template>
@@ -168,15 +191,18 @@
           <v-card-title class="d-flex align-center">
             Tags
             <v-spacer />
-            <v-btn
-              color="primary"
-              variant="text"
-              size="small"
-              prepend-icon="mdi-plus"
-              @click="addTagDialog = true"
-            >
-              Add Tag
-            </v-btn>
+            <v-tooltip text="Add Tag" location="top">
+              <template #activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                  icon="mdi-plus"
+                  color="orange-darken-2"
+                  variant="text"
+                  size="small"
+                  @click="addTagDialog = true"
+                />
+              </template>
+            </v-tooltip>
           </v-card-title>
           <v-card-text>
             <div v-if="filteredTags.length > 0" class="d-flex flex-wrap gap-2 mt-2">
@@ -239,7 +265,6 @@
             <v-switch
               v-model="tagForm.value"
               color="primary"
-              label="Value"
               inset
               hide-details
             />
@@ -339,6 +364,31 @@
   >
     Link copied to clipboard
   </v-snackbar>
+
+  <v-dialog
+    v-model="deleteItemDialog"
+    max-width="400"
+  >
+    <v-card>
+      <v-card-title>Delete item</v-card-title>
+      <v-card-text>Are you sure you want to delete this item?</v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          @click="deleteItemDialog = false"
+        >
+          Cancel
+        </v-btn>
+        <v-btn
+          color="orange-darken-2"
+          :loading="deletingItem"
+          @click="deleteItem"
+        >
+          Delete
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -374,6 +424,8 @@ export default {
       tagToDelete: null,
       deletingTag: false,
       snackbar: false,
+      deleteItemDialog: false,
+      deletingItem: false,
       rules: {
         required: value => !!value || 'Required.',
         numeric: value => !isNaN(parseFloat(value)) && isFinite(value) || 'Must be a number.'
@@ -652,6 +704,22 @@ export default {
         this.snackbar = true
       } catch (err) {
         console.error('Failed to copy text: ', err)
+      }
+    },
+    confirmDeleteItem() {
+      this.deleteItemDialog = true
+    },
+    async deleteItem() {
+      this.deletingItem = true
+      try {
+        await this.$carabassa.deleteItem(this.item.id)
+        this.deleteItemDialog = false
+        this.$router.back()
+      } catch (err) {
+        console.error('Error deleting item:', err)
+        this.$notification.alert('Failed to delete item: ' + (err.message || 'Unknown error'))
+      } finally {
+        this.deletingItem = false
       }
     }
   }
