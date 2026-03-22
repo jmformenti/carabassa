2e01a7f772a83910a2539bc23d5f87ce5fcbe3cb
Josep Maria Formentí Serra <jmformenti@gmail.com>
2026-03-22 22:11:19 +0100

    fix(frontend):keep page when back from item

--- frontend/stores/dataset.js
+++ frontend/stores/dataset.js
@@ -3,8 +3,37 @@ import { defineStore } from 'pinia'
 export const useDatasetStore = defineStore('dataset', {
   state: () => ({
     dataset: null,
-    datasetsLoaded: false
+    datasetsLoaded: false,
+    listState: {
+      datasetName: null,
+      items: [],
+      currentPage: 0,
+      totalItems: 0,
+      totalPages: 0,
+      searchString: '',
+      sortField: 'archiveTime',
+      sortDirection: 'desc',
+      scrollPosition: 0,
+      searched: false
+    }
   }),
   actions: {
+    setListState(state) {
+      this.listState = { ...this.listState, ...state };
+    },
+    resetListState() {
+      this.listState = {
+        datasetName: null,
+        items: [],
+        currentPage: 0,
+        totalItems: 0,
+        totalPages: 0,
+        searchString: '',
+        sortField: 'archiveTime',
+        sortDirection: 'desc',
+        scrollPosition: 0,
+        searched: false
+      };
+    }
   }
 });
